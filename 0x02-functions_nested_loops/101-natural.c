8910eb5ebbd32b9431b601576a840f46999818b3
FlexAlot <francis.nsithole@gmail.com>
2023-10-24 07:44:03 -0700

    corrections

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int i, z = 0;
-	
+
 	while (i < 1024)
 	{
 	if ((i % 3 == 0) || (i % 5 == 0))
@@ -21,6 +21,6 @@ int main(void)
 	i++;
 	}
 	printf("%d\n", z);
-	return (0);
 
+	return (0);
 }
